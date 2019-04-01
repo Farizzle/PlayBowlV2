e4f8cec30ae7e3b10cc3962cff9403c1bcd49544
Faris <faris.zaman@metricell.com>
2019-04-01 08:46:15 +0100

    chore: Organise project

---
+++ PlayBowlingV2/User Interface/StandardFrameCell.swift
@@ -0,0 +1,16 @@
+//
+//  StandardFrameCell.swift
+//  PlayBowlingV2
+//
+//  Created by Metricell Developer on 31/03/2019.
+//  Copyright © 2019 Metricell Developer. All rights reserved.
+//
+
+import UIKit
+
+class StandardFrameCell: UICollectionViewCell {
+    @IBOutlet var rollOne: UILabel!
+    @IBOutlet var rollTwo: UILabel!
+    @IBOutlet var frameScore: UILabel!
+    @IBOutlet var frameLabel: UILabel!
+}
