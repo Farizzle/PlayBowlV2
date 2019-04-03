0ad2759c66ee591a896c6e4f304ac42dad3ac350
Faris <faris.zaman@metricell.com>
2019-04-03 20:53:10 +0100

    feat: Convert Dates to String with format

--- PlayBowlingV2/Core/AppDelegate.swift
+++ PlayBowlingV2/Core/AppDelegate.swift
@@ -97,4 +97,16 @@ extension Array {
     }
 }
 
+extension Date {
+    func toString(withFormat format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        let myString = formatter.string(from: self)
+        let yourDate = formatter.date(from: myString)
+        formatter.dateFormat = format
+        
+        return formatter.string(from: yourDate!)
+    }
+}
+
 
